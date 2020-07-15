4aafe46e957104e3f9c2880a145d6d9c3744f1fa
Alphacode18 <skharban@purdue.edu>
2020-07-15 18:29:54 +0530

    Redundant Code Deleted

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,3 @@
-const userData = [];
-const fs = require('fs');
-
 const getRegister = (request, response, next) => {
     response.render('register', {
        pageTitle: 'Register',
@@ -8,11 +5,8 @@ const getRegister = (request, response, next) => {
  };
 
 const postRegister = (request, response, next) => {
-    userData.push(request.body);
-    fs.writeFileSync('database.json', JSON.stringify(userData));
     response.redirect('/registered');
 };
 
 exports.getRegister = getRegister;
 exports.postRegister = postRegister;
-exports.userData = userData;
--- controllers/registered.js
+++ controllers/registered.js
@@ -1,9 +1,6 @@
-const userData = require('./register').userData;
-
 const getRegistered = (request, response, next) => {
    response.render('registered', {
-       pageTitle: 'Registration Successful',
-       name: userData[userData.length - 1].name,
+       pageTitle: 'Registration Successful'
     });
  };
 
